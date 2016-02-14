83e1f5792ecea57fe525e39591ef850d3f56fb0b
Dan North <dan@dannorth.net>
2016-02-14 07:50:21 +0100

    [dn] Move Assert's implementation details out of the way

--- src/main/java/org/jgotesting/Assert.java
+++ src/main/java/org/jgotesting/Assert.java
@@ -9,19 +9,130 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class Assert {
+    /** Protect constructor since it is a static only class */
+    protected Assert() {
+    }
 
-    private static InvocationHandler delegateToStaticAssertMethod = new DelegateToStaticMethod(org.junit.Assert.class);
+    public static void assertEquals(double expected, double actual) {
+        delegate.assertEquals(expected, actual);
+    }
+
+    public static void assertNotEquals(String message, double first, double second, double delta) {
+        delegate.assertNotEquals(message, first, second, delta);
+    }
+
+    public static void assertTrue(String message, boolean condition) {
+        delegate.assertTrue(message, condition);
+    }
+
+    public static void assertEquals(String message, double expected, double actual) {
+        delegate.assertEquals(message, expected, actual);
+    }
+
+    public static void assertNull(String message, Object object) {
+        delegate.assertNull(message, object);
+    }
+
+    public static void assertNotSame(Object unexpected, Object actual) {
+        delegate.assertNotSame(unexpected, actual);
+    }
+
+    public static void assertNotEquals(Object first, Object second) {
+        delegate.assertNotEquals(first, second);
+    }
+
+    public static void assertFalse(String message, boolean condition) {
+        delegate.assertFalse(message, condition);
+    }
+
+    public static void assertNull(Object object) {
+        delegate.assertNull(object);
+    }
+
+    public static void assertNotEquals(long first, long second) {
+        delegate.assertNotEquals(first, second);
+    }
+
+    public static void assertNotEquals(double first, double second, double delta) {
+        delegate.assertNotEquals(first, second, delta);
+    }
+
+    public static void assertFalse(boolean condition) {
+        delegate.assertFalse(condition);
+    }
+
+    public static void assertNotNull(Object object) {
+        delegate.assertNotNull(object);
+    }
+
+    public static void assertEquals(String message, double expected, double actual, double delta) {
+        delegate.assertEquals(message, expected, actual, delta);
+    }
+
+    public static void assertEquals(double expected, double actual, double delta) {
+        delegate.assertEquals(expected, actual, delta);
+    }
+
+    public static void assertNotEquals(String message, Object first, Object second) {
+        delegate.assertNotEquals(message, first, second);
+    }
+
+    public static void assertNotNull(String message, Object object) {
+        delegate.assertNotNull(message, object);
+    }
+
+    public static void assertSame(Object expected, Object actual) {
+        delegate.assertSame(expected, actual);
+    }
+
+    public static void assertEquals(String message, Object expected, Object actual) {
+        delegate.assertEquals(message, expected, actual);
+    }
+
+    public static void assertNotSame(String message, Object unexpected, Object actual) {
+        delegate.assertNotSame(message, unexpected, actual);
+    }
+
+    public static void assertEquals(Object expected, Object actual) {
+        delegate.assertEquals(expected, actual);
+    }
+
+    public static void assertNotEquals(String message, long first, long second) {
+        delegate.assertNotEquals(message, first, second);
+    }
+
+    public static void assertTrue(boolean condition) {
+        delegate.assertTrue(condition);
+    }
+
+    public static void assertSame(String message, Object expected, Object actual) {
+        delegate.assertSame(message, expected, actual);
+    }
+
+    public static void assertEquals(long expected, long actual) {
+        delegate.assertEquals(expected, actual);
+    }
+
+    public static void assertEquals(String message, float expected, float actual, float delta) {
+        delegate.assertEquals(message, expected, actual, delta);
+    }
+
+    public static void assertEquals(float expected, float actual, float delta) {
+        delegate.assertEquals(expected, actual, delta);
+    }
+
+    public static void assertEquals(String message, long expected, long actual) {
+        delegate.assertEquals(message, expected, actual);
+    }
+
+    // Here's how it works
+
     private static JUnitAssertions delegate = (JUnitAssertions) Proxy.newProxyInstance(
             JUnitAssertions.class.getClassLoader(),
             new Class[] { JUnitAssertions.class },
-            delegateToStaticAssertMethod);
-
-    /**
-     * Protect constructor since it is a static only class
-     */
-    protected Assert() {
-    }
+            new DelegateToStaticMethod(org.junit.Assert.class));
 
     /**
      * tracks the signatures of all the static assert methods in {@link org.junit.Assert}
@@ -136,125 +247,4 @@ public class Assert {
         }
     }
 
-    // Everything after here is generated
-
-    public static void assertEquals(double expected, double actual) {
-        delegate.assertEquals(expected, actual);
-    }
-
-    public static void assertNotEquals(String message, double first, double second, double delta) {
-        delegate.assertNotEquals(message, first, second, delta);
-    }
-
-    public static void assertTrue(String message, boolean condition) {
-        delegate.assertTrue(message, condition);
-    }
-
-    public static void assertEquals(String message, double expected, double actual) {
-        delegate.assertEquals(message, expected, actual);
-    }
-
-    public static void assertNull(String message, Object object) {
-        delegate.assertNull(message, object);
-    }
-
-    public static void assertNotSame(Object unexpected, Object actual) {
-        delegate.assertNotSame(unexpected, actual);
-    }
-
-    public static void assertNotEquals(Object first, Object second) {
-        delegate.assertNotEquals(first, second);
-    }
-
-    public static void assertFalse(String message, boolean condition) {
-        delegate.assertFalse(message, condition);
-    }
-
-    public static void assertNull(Object object) {
-        delegate.assertNull(object);
-    }
-
-    public static void assertNotEquals(long first, long second) {
-        delegate.assertNotEquals(first, second);
-    }
-
-    public static void assertNotEquals(double first, double second, double delta) {
-        delegate.assertNotEquals(first, second, delta);
-    }
-
-    public static void assertFalse(boolean condition) {
-        delegate.assertFalse(condition);
-    }
-
-    public static void assertNotNull(Object object) {
-        delegate.assertNotNull(object);
-    }
-
-    public static void assertEquals(String message, double expected, double actual, double delta) {
-        delegate.assertEquals(message, expected, actual, delta);
-    }
-
-    public static void assertEquals(double expected, double actual, double delta) {
-        delegate.assertEquals(expected, actual, delta);
-    }
-
-    public static void assertNotEquals(String message, Object first, Object second) {
-        delegate.assertNotEquals(message, first, second);
-    }
-
-    public static void assertNotNull(String message, Object object) {
-        delegate.assertNotNull(message, object);
-    }
-
-    public static void assertSame(Object expected, Object actual) {
-        delegate.assertSame(expected, actual);
-    }
-
-    public void fail(String message) {
-        delegate.fail(message);
-    }
-
-    public void fail() {
-        delegate.fail();
-    }
-
-    public static void assertEquals(String message, Object expected, Object actual) {
-        delegate.assertEquals(message, expected, actual);
-    }
-
-    public static void assertNotSame(String message, Object unexpected, Object actual) {
-        delegate.assertNotSame(message, unexpected, actual);
-    }
-
-    public static void assertEquals(Object expected, Object actual) {
-        delegate.assertEquals(expected, actual);
-    }
-
-    public static void assertNotEquals(String message, long first, long second) {
-        delegate.assertNotEquals(message, first, second);
-    }
-
-    public static void assertTrue(boolean condition) {
-        delegate.assertTrue(condition);
-    }
-
-    public static void assertSame(String message, Object expected, Object actual) {
-        delegate.assertSame(message, expected, actual);
-    }
-
-    public static void assertEquals(long expected, long actual) {
-        delegate.assertEquals(expected, actual);
-    }
-
-    public static void assertEquals(String message, float expected, float actual, float delta) {
-        delegate.assertEquals(message, expected, actual, delta);
-    }
-
-    public static void assertEquals(float expected, float actual, float delta) {
-        delegate.assertEquals(expected, actual, delta);
-    }
-
-    public static void assertEquals(String message, long expected, long actual) {
-        delegate.assertEquals(message, expected, actual);
-    }
 }
